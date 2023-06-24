6155773aabe5e51e7454dcafad7ec17ba99f108d
Hvitrevs <venusof12pm@proton.me>
2023-06-24 17:46:50 -0400

    catching more errors, adding body, styling header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,16 @@ import { Home, CreatePost } from './pages';
 const App = () => {
   return (
     <BrowserRouter>
-    <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#e6ebf4]'>
+    <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#feeacb]'>
       <Link to="/">
         <img src= { logo } alt="logo" className= "w-28 object-contain" /></Link>
 
-      <Link to= "/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md'>Create</Link>
+      <Link to= "/create-post" className='font-inter font-medium bg-gradient-to-r from-cyan-400 to-pink-500 text-white px-4 py-2 rounded-md'>Create</Link>
+    
     </header>
+    <main className='sm: p-8 px-4 py-2 w-full bg-[#a0a0a0] min-h-[calc(100vh-73px)]'>
+      
+    </main>
     </BrowserRouter>
   )
 }
