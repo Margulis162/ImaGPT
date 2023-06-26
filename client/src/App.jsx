bae2a56583a124f95e06c59d4de4a2694caeb022
Hvitrevs <venusof12pm@proton.me>
2023-06-26 10:41:10 -0400

    added logo, styled button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,9 @@ const App = () => {
       <Link to="/">
         <img src= { logo } alt="logo" className= "w-28 object-contain" /></Link>
 
-      <Link to= "/create-post" className='font-inter font-medium bg-gradient-to-r from-cyan-400 to-pink-500 text-white px-4 py-2 rounded-md'>Create</Link>
-    
+      <Link to= "/create-post" className='font-inter font-medium bg-gradient-to-r from-cyan-400 to-pink-500 text-white px-4 py-2 rounded-full'>Create new</Link>
+      {/* <Link to= "/create-post" className='hidden sm:flex items-center font-inter font-medium w-28 space-x-4 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-4 focus:ring-sky-500 shadow-sm text-white px-4 py-2 rounded-full dark:bg-gradient-to-r from-cyan-400 to-pink-500 dark:ring-0 dark:text-slate-200 dark:highlight-white/5 dark:hover:bg-slate-900'>Create new</Link> */}
+      
     </header>
     <main className='sm: p-8 px-4 py-2 w-full bg-[#a0a0a0] min-h-[calc(100vh-73px)]'>
       
