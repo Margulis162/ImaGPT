53e547c1f38043a9838cf0033c40bafa92d8f07b
Hvitrevs <venusof12pm@proton.me>
2023-07-04 12:10:16 -0400

    resaving form

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -5,7 +5,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
     <div className='flex items-center gap-2 mb-2'>
         <label
             htmlFor={name}
-            className='block text-sm font-meddium text-gray-900'
+            className='block text-medium font-medium text-gray-600'
         >
             {labelName}
         </label>
@@ -13,7 +13,7 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
             <button
                 type='button'
                 onClick={handleSurpriseMe}
-                className='font-inter font-medium bg-gradient-to-r from-[#4db3b3] to-[#b34d99] text-white px-3 py-1 rounded-full ease-in-out delay-100 hover:-translate-y-1 hover:scale-98 duration-1000'
+                className='font-inter font-medium bg-gradient-to-r from-[#4db3b3] to-[#b34d99] text-[#d5d3d3] text-sm px-3 py-2 rounded-2xl ease-in-out delay-100 hover:-translate-y-1 hover:scale-98 duration-1000'
             >
             Suggest idea?
 
