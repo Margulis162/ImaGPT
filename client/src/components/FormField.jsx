9003edea2a3dbfbf4204a7bcb5db94cbca087f32
Hvitrevs <venusof12pm@proton.me>
2023-07-04 13:28:14 -0400

    fixed button

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -9,11 +9,12 @@ const FormField = ({ labelName, type, name, placeholder, value, handleChange, is
         >
             {labelName}
         </label>
+        {/* randomizer button bellow */}
         {isSurpriseMe && (
             <button
                 type='button'
                 onClick={handleSurpriseMe}
-                className='font-inter font-medium bg-gradient-to-r from-[#4db3b3] to-[#b34d99] text-[#d5d3d3] text-sm px-3 py-2 rounded-2xl ease-in-out delay-100 hover:-translate-y-1 hover:scale-98 duration-1000'
+                className='font-inter font-medium bg-gradient-to-b from-[#aab4b4] border-2 border-[#adaeae] border-b-[#8e9191] text-[#fbf8f8] text-sm px-3 py-2 rounded-2xl ease-in-out delay-100 hover:-translate-y-1 hover:scale-95 duration-1000'
             >
             Suggest idea?
 
