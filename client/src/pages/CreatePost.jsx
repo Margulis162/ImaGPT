539e565078f0cb0f0a12a95c9424dfe8b619322b
Hvitrevs <venusof12pm@proton.me>
2023-07-06 17:40:12 -0400

    button UI

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -78,13 +78,14 @@ const generateImage = () => {
           </div>
         </div>
 
-        <div className='mt-5 flex gap-5 text-[#666666] '>
+        <div className='mt-5 flex gap-5'>
           <button
             type='button'
             onClick={generateImage}
+            className='font-inter font-bold bg-gradient-to-b from-[#aeaeae] to-[#c0c0c0] border-2 border-[#c2c2c2] border-b-[#a1a1a1] text-[#7b7a7a] text-sm px-3 py-2 rounded-2xl shadow-md' 
 
           >
-            {generateImage ? 'Generating. . .' : 'Generate' }
+            {generatingImg ? 'Generating. . .' : 'Generate' }
 
           </button>
         </div>
