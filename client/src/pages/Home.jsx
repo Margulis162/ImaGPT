6daa4dca8eb0bb3efe2dffb8fd8fe5de0f48d7f6
Hvitrevs <venusof12pm@proton.me>
2023-07-05 21:43:16 -0400

    polishing UI more

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,15 +20,13 @@ const Home = () => {
   return (
     <section className='max-w-7x1 mx-auto'>
       <div>
-        <h1 className='font-bold text-[#3c3840] text-[32px]'>Most Popular Generated Images</h1>
+        <h1 className='font-bold text-[#3c3840] text-[32px]'>Discover Most Popular Generated Images</h1>
         <p className='mt-2 text-[#4f5152] text-[1.3rem] max-w-[500px]'> See what other people generated with AI</p>
       </div>
 
       <div className='mt-16'>
         <FormField />
       </div>
-
-{/* this gonna be loader? */}
 
       <div className='mt-10'>
         {loading ? (
@@ -51,7 +49,7 @@ const Home = () => {
             ) : (
               <RenderCards
               data={[]}
-              title="I couldn't find any posts. Give me another shot!"
+              title="No posts found"
               />
             )}
           </div>
